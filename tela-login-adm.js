1a4d077e429d4fe978af8e58ea520c5a6329ac67
julianasousaaa <julianadesousasilva96@gmail.com>
2025-07-03 18:56:37 -0300

    alteraçoes

---
+++ tela-login-adm.js
@@ -0,0 +1,21 @@
+const form = document.getElementById('adm-login-form');
+const passwordInput = document.getElementById('admin-password');
+const message = document.getElementById('login-message');
+
+const senhaCorreta = "delegacia123";
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  if (passwordInput.value === senhaCorreta) {
+    message.style.color = 'green';
+    message.textContent = 'Acesso autorizado!';
+    setTimeout(() => {
+      window.location.href = "painel-adm.html"; 
+    }, 1000);
+  } else {
+    message.style.color = 'red';
+    message.textContent = 'Senha incorreta.';
+    passwordInput.value = '';
+  }
+});
